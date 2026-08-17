ceaffdde5f569e62770bb378bc59d9cfa825e8a3
Sakshi Singh <Sakshi.4.Singh@coforge.com>
2026-08-17 14:34:08 +0530

    Added Google Authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,28 @@
 import { Routes } from '@angular/router';
 import {EmployeeList} from './components/employee-list/employee-list';
 import {EmployeeDetails } from './components/employee-details/employee-details';
+import { Login } from './components/login/login';
+import { authGuard } from './guards/auth.gaurd';
 
 
 export const routes: Routes = [
     {
         path:'',
-        component:EmployeeList
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'employee-list',
+        component:EmployeeList,
+        canActivate:[authGuard]
     },
     {
          path:'employee/:id',
-        component:EmployeeDetails
+        component:EmployeeDetails,
+        canActivate:[authGuard]
     }
 ];
